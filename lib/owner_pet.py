ccbbd38966e1e8f7c6e32eb6fb0d617849969ff2
Edith Thaba <writersherry5@gmail.com>
2024-03-10 14:49:08 +0300

    Done with Assignment

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,36 @@
-class Pet:
-    pass
+#Define an Owner class and pass into the constructor a name argument
+#Define a Pet and pass into the constructor its name, pet_type, and an optional owner
+#Define a class variable PET_TYPES and validate that the pet_type is one of those types in the init method
+#Define a class variable all that stores all instances of the Pet class
+#write a method called pets(self) that returns a full list of the owner's pet
+#In the Owner class, write a method called add_pet(self, pet) 
+#returns a sorted list of pets by their names:
 
 class Owner:
-    pass
+    def __init__(self, name):
+        self.name = name
+
+    def pets(self):
+        return [pet for pet in Pet.all if isinstance(pet.owner, Owner) and pet.owner == self]
+
+    def add_pet(self, pet):
+        if not isinstance(pet, Pet):
+            raise Exception("The pet must be of type Pet.")
+        pet.owner = self
+
+    def get_sorted_pets(self):
+        sorted_pets = sorted(self.pets(), key=lambda x: x.name)
+        return sorted_pets
+
+
+class Pet:
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+    all = []
+
+    def __init__(self, name, pet_type, owner=None):
+        if pet_type not in self.PET_TYPES:
+            raise Exception("Invalid pet type.")
+        self.name = name
+        self.pet_type = pet_type
+        self.owner = owner
+        self.all.append(self)
